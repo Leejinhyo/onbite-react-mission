ed565d52f429e1ca6cff1f8adbdfcc46dad4d937
ljh <jinhyo6407@naver.com>
2024-04-03 14:49:12 +0900

    -

--- day10/day10_mission/src/Welcome.jsx
+++ day10/day10_mission/src/Welcome.jsx
@@ -1,12 +1,6 @@
 const Welcome = ({ name, isMember }) => {
   return (
-    <>
-      {isMember ? (
-        <div>{`${name}님 다시 오셨군요.`}</div>
-      ) : (
-        <div>{`${name}님 가입하시겠어요?`}</div>
-      )}
-    </>
+    <>{isMember ? `${name}님 다시 오셨군요.` : `${name}님 가입하시겠어요?`}</>
   );
 };
 
